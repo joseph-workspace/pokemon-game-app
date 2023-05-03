3803c81eca8feaefd4df16147e7dfd58ba7ab62b
jaromoyler31 <jarroyler@gmail.com>
2023-05-03 08:36:49 -0600

    Update gotten father

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { take } from 'rxjs/operators';
+import { __propKey } from 'tslib';
 
 @Component({
   selector: 'app-tab2',
@@ -9,43 +10,39 @@ import { take } from 'rxjs/operators';
 })
 export class Tab2Page implements OnInit {
 
-  url = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
+  url = 'https://pokeapi.co/api/v2/pokemon/';
   list: any[] = [];
-  filteredList: any[] = [];
+  pokemonnumberlist: any[] = [4, 1, 7, 95, 25, 18, 92]
   isLoading = false;
-  avatarButtonStyle = {
-    'height': '60vw',
-    'width': '60vw',
-    'max-height': '200px',
-    'max-width': '200px',
-    'margin': '0 auto',
-    'background-color': 'white',
-    'border-radius': '50%',
-    'box-shadow': '0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.3)'
-  };
+
 
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit() {
-    this.isLoading = true;
-    this.http.get<any>(this.url)
-      .pipe(take(1))
-      .subscribe(pokeData => {
-        this.list = pokeData.results;
+    this.getData()
+    
+  }
+
+  getData(){
+    for (let q = 0; q < this.pokemonnumberlist.length; q++) {
+      this.isLoading = true
+      this.http.get<any>(this.url + this.pokemonnumberlist[q])
+        .pipe(take(1))
+        .subscribe(pokeData => {
+          this.list = pokeData
+          this.isLoading = false;
+          console.log(this.list)
+        });
         this.list.sort((a: any, b: any) => a.name > b.name ? 1 : -1);
-        this.filteredList = [...this.list];
-        this.isLoading = false;
-        console.log(this.filteredList)
-      });
-      
+    }
   }
 
   filterItems(event: any) {
     this.isLoading = true;
     const text = event.srcElement.value.trim().toLowerCase();
-    this.filteredList = text
+    this.list = text
       ? this.list.filter(item => item.name.toLowerCase().includes(text))
       : [...this.list];
     this.isLoading = false;
